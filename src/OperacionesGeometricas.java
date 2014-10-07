e50aaf8f69cd55e7f4aac4e3ae863ba3090eb6b6
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-10-07 09:19:38 +0200

    constructor por parametro

--- src/OperacionesGeometricas.java
+++ src/OperacionesGeometricas.java
@@ -24,24 +24,24 @@ public class OperacionesGeometricas {
 		if ((opcion.equalsIgnoreCase("a"))||(opcion.equalsIgnoreCase("b"))){
 			if (opcion.equalsIgnoreCase("a")){
 				double radio=0;
-				Circulo c1=new Circulo();
 				System.out.println("Circulo");
 				System.out.println("Introduce el radio:");
 				radio=tecla1.nextDouble();
-				c1.setRadio(radio);
+				Circulo c1=new Circulo(radio);
+//				c1.setRadio(radio);
 				System.out.println("Area: "+c1.calculaArea());
 				System.out.println("Perimetro: "+c1.calculaPerimetro());
 			}
 			if (opcion.equalsIgnoreCase("b")){
 				double base=0; double altura=0;
-				Rectangulo r1=new Rectangulo();
 				System.out.println("Rectangulo");
 				System.out.println("Introduce la base:");
 				base=tecla1.nextDouble();
-				r1.setBase(base);
 				System.out.println("Introduce la altura:");
 				altura=tecla1.nextDouble();
-				r1.setAltura(altura);
+				Rectangulo r1=new Rectangulo(base,altura);
+				//r1.setBase(base);
+				//r1.setAltura(altura);
 				System.out.println("Area: "+r1.calculaArea());
 				System.out.println("Perimetro: "+r1.calculaPerimetro());
 			}
